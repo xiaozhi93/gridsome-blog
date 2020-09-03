4c01e129dfcac174b918feca4aa6b89d23d5b7e7
windfly <974589985@qq.com>
2020-09-03 14:10:18 +0800

    添加开源项目数据

--- gridsome.server.js
+++ gridsome.server.js
@@ -12,23 +12,16 @@ const axiosQuerstList = [
   axios('https://api.github.com/users/xiaozhi93/repos')
 ]
 module.exports = function (api) {
-  // https://api.github.com/users/GitHub-Laziji
-  // https://api.github.com/user?access_token=96066f7ef7920cfaeb083bb11ea0836ae2737174
-  // https://api.github.com/users/GitHub-Laziji/repos?page=1&per_page=5
-  // https://api.github.com/users/GitHub-Laziji/gists?page=1&per_page=5
-  // https://api.github.com/users/GitHub-Laziji/followers?page=1&per_page=9
-  // https://api.github.com/users/GitHub-Laziji/following?page=1&per_page=9
-  // https://api.github.com/users/GitHub-Laziji/gists?page=1&per_page=1
   api.loadSource(async ({ addCollection }) => {
     const [res1, res2, res3, res4] = await Promise.all(axiosQuerstList)
-    console.log(res1, '1111111')
     const userCollection = addCollection('User')
     const followersCollection = addCollection('Followers')
     const followingCollection = addCollection('Following')
-
+    const reposCollection = addCollection('repo')
     userCollection.addNode(res1.data)
     followersCollection.addNode(res2.data)
     followingCollection.addNode(res3.data)
+    reposCollection.addNode(res4.data)
   })
 
   api.createPages(({ createPage }) => {
